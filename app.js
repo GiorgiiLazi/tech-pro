adc0611ff6e27b81c0c68daa741395cdc7788bb7
george <malazi140@gmail.com>
2023-10-10 16:54:18 +0300

    finished navbar

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ const modal = document.querySelector('.modal-background')
 const removeBtnOne = document.getElementById("remove1")
 const removeBtnTwo = document.getElementById("remove2")
 const banner = document.querySelector(".banner")
+const navbar = document.querySelector(".navbar")
 
 showBanner.addEventListener('click', ()=>{
     modal.style.display = ""
 })
 removeBtnOne.addEventListener('click', ()=>{
     banner.style.display = "none"
+    navbar.style.top = '0px'
 })
 removeBtnTwo.addEventListener('click', ()=>{
     modal.style.display = "none"
