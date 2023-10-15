34c4a8edb101b34359ed827fc95ccf97a59cb74d
george <malazi140@gmail.com>
2023-10-15 18:17:06 +0300

    added subscribe form, refactured left panel, added qr

--- app.js
+++ app.js
@@ -7,6 +7,12 @@ const navbar = document.querySelector(".navbar")
 const leftPanel = document.querySelector(".left-panel")
 const rightArrow = document.querySelector(".right-arrow")
 const leftArrow = document.querySelector(".left-arrow")
+const subForm = document.querySelector(".subForm")
+const subModal = document.querySelector('.subBackground')
+const mail = document.getElementById('mail')
+const subName = document.getElementById('name')
+const subModalText = document.querySelector(".sub-modal-text")
+const removeBtnThree = document.getElementById("remove3")
 
 showBanner.addEventListener('click', ()=>{
     modal.style.display = ""
@@ -18,15 +24,30 @@ removeBtnOne.addEventListener('click', ()=>{
 removeBtnTwo.addEventListener('click', ()=>{
     modal.style.display = "none"
 })
-rightArrow.addEventListener('click', ()=>{
-    leftPanel.style.width = '100%'
-    leftPanel.style.opacity = '100%'
-    rightArrow.style.display = "none"
-    leftArrow.style.display = "inline-block"
+leftPanel.addEventListener('click', ()=>{
+    if(leftArrow.style.display == "none"){
+        leftPanel.style.width = '180px'
+        rightArrow.style.display = "none"
+        leftArrow.style.display = "inline-block"
+    }
+    else if(rightArrow.style.display == "none"){
+        leftPanel.style.width = '25px'
+        rightArrow.style.display = "inline-block"
+        leftArrow.style.display = "none"
+    }
 })
-leftArrow.addEventListener('click', ()=>{
-    leftPanel.style.width = '25px'
-    leftPanel.style.opacity = '50%'
-    rightArrow.style.display = "inline-block"
-    leftArrow.style.display = "none"
+subForm.addEventListener('submit', (e)=>{
+    e.preventDefault();
+
+    subModal.style.display = "block"
+    
+    subModalText.textContent = `Thank you for subscribing to our newsletter ${subName.value.toUpperCase().trim()}. From now on you will receive information on discounts and our special offers to your Email address ${mail.value.trim()}!!!`
+
+    console.log(subName.value.toUpperCase().trim())
+    console.log(mail.value.trim())
+    
+    subForm.reset()
+})
+removeBtnThree.addEventListener('click', ()=>{
+    subModal.style.display = "none"
 })
