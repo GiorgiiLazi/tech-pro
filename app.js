072346c09e5a7f531156da3bb8823cd45648661a
george <malazi140@gmail.com>
2023-10-18 15:47:15 +0300

    added about and sign in section

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const showBanner = document.getElementById("showBanner")
+const bannerDate = document.querySelector("#date")
 const modal = document.querySelector('.modal-background')
 const removeBtnOne = document.getElementById("remove1")
 const removeBtnTwo = document.getElementById("remove2")
@@ -12,36 +13,53 @@ const subModal = document.querySelector('.subBackground')
 const mail = document.getElementById('mail')
 const subName = document.getElementById('name')
 const subModalText = document.querySelector(".sub-modal-text")
+const subModalText2 = document.querySelector(".sub-modal-text2")
 const removeBtnThree = document.getElementById("remove3")
+const mainSection = document.querySelector(".main-section")
+const aboutSection = document.querySelector('.about')
+const aboutRef = document.getElementById('aboutRef')
+const aboutBackBtn = document.querySelector('#aboutBackBtn')
+const signInSection = document.querySelector('.sign-in')
+const signInForm = document.querySelector('.sign-form')
+const userName = document.getElementById('username')
+const phoneNumber = document.getElementById('phoneNumber')
+const formEmail = document.getElementById('form-email')
+const formPassword1 = document.getElementById('form-password1')
+const formPassword2 = document.getElementById('form-password2')
+const checkSendAds = document.getElementById('sendAds')
+const formBackBtn = document.querySelector('#formBackBtn')
+const signInRef = document.getElementById('signInRef')
 
+
+
+
+// assign date to banner
+const date = new Date()
+
+bannerDate.textContent =  date.toDateString()
+
+// show banner
 showBanner.addEventListener('click', ()=>{
-    modal.style.display = ""
+    modal.classList.remove('not-show')
 })
+
+// remove banners
 removeBtnOne.addEventListener('click', ()=>{
-    banner.style.display = "none"
+    banner.classList.add('not-show')
     navbar.style.top = '0px'
 })
 removeBtnTwo.addEventListener('click', ()=>{
-    modal.style.display = "none"
+    modal.classList.add('not-show')
 })
-leftPanel.addEventListener('click', ()=>{
-    if(leftArrow.style.display == "none"){
-        leftPanel.style.width = '180px'
-        rightArrow.style.display = "none"
-        leftArrow.style.display = "inline-block"
-    }
-    else if(rightArrow.style.display == "none"){
-        leftPanel.style.width = '25px'
-        rightArrow.style.display = "inline-block"
-        leftArrow.style.display = "none"
-    }
-})
+
+// subscription form
 subForm.addEventListener('submit', (e)=>{
     e.preventDefault();
 
-    subModal.style.display = "block"
+    subModal.classList.remove('not-show')
     
-    subModalText.textContent = `Thank you for subscribing to our newsletter ${subName.value.toUpperCase().trim()}. From now on you will receive information on discounts and our special offers to your Email address ${mail.value.trim()}!!!`
+    subModalText.textContent = `Thank you for subscribing to our newsletter ${subName.value.toUpperCase().trim()}!!!`
+    subModalText2.textContent = `From now on you will receive information on discounts and our special offers to your Email address ${mail.value.trim()}!!!`
 
     console.log(subName.value.toUpperCase().trim())
     console.log(mail.value.trim())
@@ -49,5 +67,41 @@ subForm.addEventListener('submit', (e)=>{
     subForm.reset()
 })
 removeBtnThree.addEventListener('click', ()=>{
-    subModal.style.display = "none"
+    subModal.classList.add('not-show')
+})
+
+// show about
+
+aboutRef.addEventListener('click', ()=>{
+    mainSection.classList.add('not-show')
+    aboutSection.classList.remove('not-show')
+})
+
+aboutBackBtn.addEventListener('click', ()=>{
+    mainSection.classList.remove('not-show')
+    aboutSection.classList.add('not-show')
+})
+
+// sign in form
+signInRef.addEventListener('click', ()=>{
+    mainSection.classList.add('not-show')
+    signInSection.classList.remove('not-show')
+})
+
+formBackBtn.addEventListener('click', ()=>{
+    mainSection.classList.remove('not-show')
+    signInSection.classList.add('not-show')
+})
+
+signInForm.addEventListener('submit', (e)=>{
+    e.preventDefault();
+
+    console.log(userName.value)
+    console.log(phoneNumber.value)
+    console.log(formEmail.value)
+    console.log(formPassword1.value)
+    console.log(formPassword2.value)
+    console.log(checkSendAds.checked)
+    
+    subForm.reset()
 })
